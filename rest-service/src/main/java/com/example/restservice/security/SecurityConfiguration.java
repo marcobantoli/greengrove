048e202cc5955e4c4e282be5b57236f28e51953b
Marco Bantolino <marcobantoli@gmail.com>
2024-09-01 10:14:43 -0400

    Change cors config

--- rest-service/src/main/java/com/example/restservice/security/SecurityConfiguration.java
+++ rest-service/src/main/java/com/example/restservice/security/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
