a6a3899b03919ce79804981157af202687c4272a
Marco Bantolino <marcobantoli@gmail.com>
2024-12-30 22:11:30 -0500

    Fix linter errors

--- frontend/src/__tests__/Footer.test.jsx
+++ frontend/src/__tests__/Footer.test.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Footer from '../components/Footer';
 
