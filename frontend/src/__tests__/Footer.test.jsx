749817221c4409f50eac2aeec33a6ec967325308
Marco Bantolino <marcobantoli@gmail.com>
2024-12-30 21:58:21 -0500

    Create test

---
+++ frontend/src/__tests__/Footer.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Footer from '../components/Footer';
+
+describe('Footer Component', () => {
+  test('renders the Green Grove section', () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+  });
+});
